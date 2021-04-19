dd581378e360aafda4a28dff4b65b37de8da4cd8
Jo <joannadawnmiller@gmail.com>
2021-04-19 18:26:48 -0400

    add buttons to suggestions
    Co-authored-by: Dani Renner <danijrenner@gmail.com>

--- src/components/Suggestions.js
+++ src/components/Suggestions.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Image, ListGroup } from 'react-bootstrap';
+import { Card, Image, ListGroup, Button } from 'react-bootstrap';
 import fakePerson from './../img/fake-person.png'
 import fakePerson2 from './../img/fake-person2.png'
 import fakePerson3 from './../img/fake-person3.png'
@@ -11,9 +11,9 @@ function Suggestions() {
       <Card.Header>People you may know</Card.Header>
       <ListGroup as="ul">
         <Card.Body>
-          <ListGroup.Item><Image src={ fakePerson } style={{ height: '50px' }} roundedCircle />fake_user_1</ListGroup.Item>
-          <ListGroup.Item><Image src={ fakePerson2 } style={{ height: '50px' }} roundedCircle />fake_user_2</ListGroup.Item>
-          <ListGroup.Item><Image src={ fakePerson3 } style={{ height: '50px' }} roundedCircle />fake_user_3</ListGroup.Item>
+          <ListGroup.Item><Image src={ fakePerson } style={{ height: '50px' }} roundedCircle /> fake_user_1<Button variant="outline-info" size='sm'>Add Friend</Button></ListGroup.Item>
+          <ListGroup.Item><Image src={ fakePerson2 } style={{ height: '50px' }} roundedCircle /> fake_user_2<Button variant="outline-info" size='sm'>Add Friend</Button></ListGroup.Item>
+          <ListGroup.Item><Image src={ fakePerson3 } style={{ height: '50px' }} roundedCircle /> fake_user_3<Button variant="outline-info" size='sm'>Add Friend</Button></ListGroup.Item>
         </Card.Body>
       </ListGroup>
     </Card>
